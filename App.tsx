d5571e7a668f427dec58c267d268098632f4b1f9
arnoldc <arnold.camas@gmail.com>
2021-11-02 19:40:09 +0800

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => {
+        return {
+            shouldShowAlert: true
+        } 
+    }
+})
 
 export default function App() {
+    const [pushToken, setPushToken] = useState<string>();
+
+    useEffect(async () => {
+          const data = await Notifications.getExpoPushTokenAsync();
+          setPushToken(data.data)
+    }, [])
+
+    useEffect(() => {
+        const backgroundSubscription = Notifications.addNotificationResponseReceivedListener(response => {
+            console.log('background ', response)
+        });
+
+          const foregroundSubscription = Notifications.addNotificationReceivedListener(notification => {
+              console.log('foreground', notification)
+          });
+
+          return () => {
+            foregroundSubscription.remove();
+            backgroundSubscription.remove();
+          }
+    }, [])
+
+  const triggerNotificationHandler = () => {
+        // Notifications.scheduleNotificationAsync({
+        //     content: {
+        //        title: 'My first local notification',
+        //        body: 'this is the first local notification we are sending',
+        //        data: {
+        //           mydata: 'hey yo'
+        //        }
+        //     },
+        //     trigger: {
+        //         seconds: 10
+        //     }
+        // });
+        fetch('https://exp.host/--/api/v2/push/send', {
+            method: 'POST',
+             headers: {
+                  'Accept': 'application/json',
+                  'Accept-Encoding': 'gzip, deflate',
+                  'Content-Type': 'application/json'
+             },
+             body: JSON.stringify({
+                to: pushToken,
+                data: {
+                    mydata: 'hey hey'
+                },
+                title: 'sent via the app',
+                body: 'sent via the app body!!!'
+             })
+        })
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Pressable onPress={triggerNotificationHandler} style={styles.button}>
+            <Text> Trigger Notification</Text>
+        </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +84,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    padding : 10,
+    backgroundColor: '#ff0',
+    borderRadius: 5,
+    marginTop: 20,
+  }
 });
